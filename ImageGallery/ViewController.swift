f3ddbaf35ee7beff5c05ce9943168136ae4ba331
roie-malykin-healthy <roie-malykin@healthy.io>
2022-05-24 14:10:51 +0300

    innitial commit for branch

--- ImageGallery/ViewController.swift
+++ ImageGallery/ViewController.swift
@@ -11,5 +11,6 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Print for commit ,  delete this !!!")
     }
 }
