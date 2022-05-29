708c9109bd759d1706af3aa6922c33ff8239d4a5
roie-malykin-healthy <roie-malykin@healthy.io>
2022-05-29 11:36:44 +0300

    inited a custom view class MyTableViewCell

--- ImageGallery/Controllers/ImageCollectionGalleryController.swift
+++ ImageGallery/Controllers/ImageCollectionGalleryController.swift
@@ -7,20 +7,58 @@
 
 import UIKit
 
-final class ImageCollectionGalleryController: UIViewController {
+final class ImageCollectionGalleryController: UIViewController, UICollectionViewDropDelegate {
+    @IBOutlet private weak var dropZoneView: UIView!
     @IBOutlet private weak var imageCollection: UICollectionView!
-    
     var dataSource: UICollectionViewDataSource?
     var delegate: UICollectionViewDelegate?
+    
+    //MARK: Drag interactions
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        <#code#>
+        
+    }
     // Starting a drag to rearange Cells
     func dragInteraction(_ interaction: UIDragInteraction, itemsForBegginning session: UIDragSession) -> [UIDragItem] {
         var draggedList = [UIDragItem]()
         return draggedList
     }
-    // Creatinh UIDragItems
+    // Creating UIDragItems
     // 1)  let dragItem = UIDragItem(itemProvider: NSitemProvider(object: provider))
     // 2) dragItem.localObject: Any = someObject
+    // MARK: - UIDropInteractionDelegate
+
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    }
     
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    var imageFetcher: ImageFetcher!
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        imageFetcher = ImageFetcher() { (url, image) in
+            DispatchQueue.main.async {
+                self.emojiArtBackgroundImage = image
+            }
+        }
+        
+        session.loadObjects(ofClass: NSURL.self) { nsurls in
+            if let url = nsurls.first as? URL {
+                self.imageFetcher.fetch(url)
+            }
+        }
+        session.loadObjects(ofClass: UIImage.self) { images in
+            if let image = images.first as? UIImage {
+                self.imageFetcher.backup = image
+            }
+        }
+    
+    
+    
+    // MARK: - Life cycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Drop action config
---
+++ ImageGallery/MyViews/MyTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.swift
+//  ImageGallery
+//
+//  Created by Roie Malykin on 29/05/2022.
+//
+
+import UIKit
+/// This class is a custom table view cell that holds a editable TextView for my ImageGalleryViewController
+/// based on this video https://www.youtube.com/watch?v=fbjrGIXB_m8
+class MyTableViewCell: UITableViewCell {
+// 
+}
